04e2483f31d620110dcdfdd2b3c358abfba24c07
aleffstampini <aleff.stampini@bandtec.com.br>
2023-01-01 06:38:05 -0300

    build: alterando rota estática de arquivos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 const home = '../src/app/index';
 
-app.use(express.static(__dirname + '/src/app'));
+app.use(express.static(__dirname + '/src'));
 
 app.set('view engine', 'ejs');
 
